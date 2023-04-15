62384bff499839a6ec2ac103b95e9f7a957ab8ae
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-04-15 21:29:39 +0300

    Урок 24

--- math_3d.h
+++ math_3d.h
@@ -1,30 +1,12 @@
-/*
-
-	Copyright 2010 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #ifndef MATH_3D_H
 #define	MATH_3D_H
 
 #include <stdio.h>
 #include <math.h>
 
-#define ToRadian(x) ((x) * M_PI / 180.0f)
-#define ToDegree(x) ((x) * 180.0f / M_PI)
-const double M_PI = 3.14;
+#define ToRadian(x) ((x) * 3.14f / 180.0f)
+#define ToDegree(x) ((x) * 180.0f / 3.14f)
+
 struct Vector2i
 {
     int x;
@@ -108,8 +90,8 @@ struct Vector3f
 inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
 {
     Vector3f Ret(l.x + r.x,
-                 l.y + r.y,
-                 l.z + r.z);
+        l.y + r.y,
+        l.z + r.z);
 
     return Ret;
 }
@@ -117,8 +99,8 @@ inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
 inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
 {
     Vector3f Ret(l.x - r.x,
-                 l.y - r.y,
-                 l.z - r.z);
+        l.y - r.y,
+        l.z - r.z);
 
     return Ret;
 }
@@ -126,8 +108,8 @@ inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
 inline Vector3f operator*(const Vector3f& l, float f)
 {
     Vector3f Ret(l.x * f,
-                 l.y * f,
-                 l.z * f);
+        l.y * f,
+        l.z * f);
 
     return Ret;
 }
@@ -139,7 +121,7 @@ public:
     float m[4][4];
 
     Matrix4f()
-    {        
+    {
     }
 
 
@@ -155,12 +137,12 @@ public:
     {
         Matrix4f Ret;
 
-        for (unsigned int i = 0 ; i < 4 ; i++) {
-            for (unsigned int j = 0 ; j < 4 ; j++) {
+        for (unsigned int i = 0; i < 4; i++) {
+            for (unsigned int j = 0; j < 4; j++) {
                 Ret.m[i][j] = m[i][0] * Right.m[0][j] +
-                              m[i][1] * Right.m[1][j] +
-                              m[i][2] * Right.m[2][j] +
-                              m[i][3] * Right.m[3][j];
+                    m[i][1] * Right.m[1][j] +
+                    m[i][2] * Right.m[2][j] +
+                    m[i][3] * Right.m[3][j];
             }
         }
 
@@ -183,12 +165,11 @@ struct Quaternion
 
     void Normalize();
 
-    Quaternion Conjugate();  
- };
+    Quaternion Conjugate();
+};
 
 Quaternion operator*(const Quaternion& l, const Quaternion& r);
 
 Quaternion operator*(const Quaternion& q, const Vector3f& v);
 
 #endif	/* MATH_3D_H */
-
